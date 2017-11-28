1e5b4d3f686e9beb428331468ce17fd686f3ab21
c0nniewang <connie.wang303@gmail.com>
2017-11-28 10:07:30 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,6 @@ class StudentsController < ApplicationController
   end
 
   def create
-    binding.pry
     @student = Student.new
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
